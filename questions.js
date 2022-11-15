0146901111e9c1fc038613f737145e2eb288006f
febitriwahyu <febitriwahyu@gdriveme.id>
2022-11-16 05:24:46 +0700

    first commit

---
+++ questions.js
@@ -0,0 +1,91 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "fungsi produksi bagi produsen adalah untuk...",
+    answer: "mengomnbinasikan faktor produksi",
+    options: [
+      "memperbanyak faktor produksi",
+      "memperkecil faktor produksi",
+      "mengombinasikan faktor produksi",
+      "memperbanyak salah satu faktor dari produksi"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Faktor internal yang memengaruhi tingkat konsumsi rumah tangga keluarga adalah...?",
+    answer: "dorongan untuk mencapai hidup yang lebih baik",
+    options: [
+      "perkembangan adat istiadat",
+      "kemajuan teknologi dan kebudayaan",
+      "kebudayaan yang semakin maju dan berkembang",
+      "dorongan untuk mencapai hidup yang lebih baik"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Di bawah ini yang termasuk faktor produksi alam sebagai berikut, kecuali....?",
+    answer: "tenaga kerja",
+    options: [
+      "timah",
+      "batubara",
+      "emas",
+      "tenaga kerja"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Cara penyaluran dengan menggunakan satu otulet saja disebut...?",
+    answer: "eklusif",
+    options: [
+      "eklusif",
+      "satu arah",
+      "selektif",
+      "menyeluruh"
+    ]
+  },
+   {
+    numb: 6,
+    question: "bahasa yang cocok untuk data science?",
+    answer: "python",
+    options: [
+      "javascript",
+      "html",
+      "python",
+      "ruby"
+    ]
+   }
+
+
+
+
+
+
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
